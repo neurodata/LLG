b0f90000c8e0d1a8318e1f7ae63c52a15aec5c80
Runze Tang <tangrunze@gmail.com>
2016-03-15 12:27:10 -0400

    Analysis_brute updated.

--- Code/R/analysis_brute.R
+++ Code/R/analysis_brute.R
@@ -5,17 +5,17 @@ setwd("/Users/Runze/Documents/GitHub/LLG/Code/R")
 
 library("igraph")
 
-par(mfrow=c(2,2));
+par(mfrow=c(2, 2));
 
 dataName = "CPAC200"
 n = 200;
 
-for (m in c(2, 5, 10, 15)) {
-  load(paste("../../Result/", dataName, "_", n, "_brute_", m, ".RData", sep =""))
+for (m in 2:5) {
+  load(paste("../../Result/", "result_", dataName, "_", n, "_brute_", m, ".RData", sep =""))
   
-  plot(2:99, type="n", ylim = c(0, 0.05), xlab="embedding dimension", ylab="MSE")
-  points(rowMeans(err_A_Bar), type="l", col="red")
-  points(rowMeans(err_P_Hat), type="l", col="blue")
+  plot(dVec, type="n", ylim = c(0.0005, 0.0015), xlab="embedding dimension", ylab="MSE")
+  points(rowMeans(error_A_bar), type="l", col="red")
+  points(rowMeans(error_P_hat), type="l", col="blue")
   title(paste("m = ", m, sep=""))
 }
 
