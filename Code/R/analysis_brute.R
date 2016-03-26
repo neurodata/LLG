32d5ded052a5a5716b472a831d73bedc0f9c4425
Runze Tang <tangrunze@gmail.com>
2016-03-26 00:44:54 -0400

    Multiple plot with shared legend updated.

--- Code/R/analysis_brute.R
+++ Code/R/analysis_brute.R
@@ -12,39 +12,6 @@ dataName = "CPAC200"
 
 mVec = c(1, 2, 5, 10)
 
-# m = 2
-# isSVD = 1
-# 
-# if (isSVD) {
-#   fileName = paste("../../Result/result_", dataName, "_brute_", "_", m, "_svd.RData", sep="")
-# } else {
-#   fileName = paste("../../Result/result_", dataName, "_brute_", "_", m, "_eig.RData", sep="")
-# }
-# load(fileName)
-# 
-# plot(dVec, type="n", ylim = c(min(min(error_P_hat),min(error_A_bar)), max(error_P_hat)), xlab="embedding dimension", ylab="MSE")
-# points(rep(mean(error_A_bar), length(dVec)), type="l", col="red")
-# points(rowMeans(error_P_hat), type="l", col="blue")
-# title(paste("m = ", m, sep=""))
-
-# for (m in 2:5) {
-#   load(paste("../../Result/", "result_", dataName, "_", n, "_brute_", m, ".RData", sep =""))
-#   
-#   plot(dVec, type="n", ylim = c(0.0005, 0.0015), xlab="embedding dimension", ylab="MSE")
-#   points(rowMeans(error_A_bar), type="l", col="red")
-#   points(rowMeans(error_P_hat), type="l", col="blue")
-#   title(paste("m = ", m, sep=""))
-# }
-
-# colorVec = c("red","blue","green")
-# lineWidth = 3
-# 
-# m = matrix(c(1,2,3,4,5,5),nrow = 3,ncol = 2, byrow = TRUE)
-# layout(mat = m, heights = c(0.4,0.4,0.2))
-# 
-# # par(mar = c(2,2,1,1))
-# 
-# mVec = c(1, 2, 5, 10)
 yMin = Inf
 yMax = 0
 for (m in mVec) {
@@ -62,39 +29,6 @@ for (m in mVec) {
 }
 yMax = yMax*1.1
 yMin = yMin*0.9
-
-# 
-# 
-# for (m in mVec) {
-#   par(mar = c(2,2,1,1))
-#   
-#   # SVD
-#   fileName = paste("../../Result/result_", dataName, "_brute_", "_", m, "_svd.RData", sep="")
-#   load(fileName)
-#   plot(dVec, type="n", ylim = c(0, yMax), xlab="embedding dimension", ylab="MSE")
-#   points(rep(mean(error_A_bar), length(dVec)), type="l", col=colorVec[1], lwd=lineWidth)
-#   points(rowMeans(error_P_hat), type="l", col=colorVec[2], lwd=lineWidth)
-#   
-#   # Eigen-decomposition
-#   fileName = paste("../../Result/result_", dataName, "_brute_", "_", m, "_eig.RData", sep="")
-#   load(fileName)
-#   points(rowMeans(error_P_hat), type="l", col=colorVec[3], lwd=lineWidth)
-#   
-# #   legend("topright", legend=c("Abar", "Phat_SVD", "Phat_Eig"), 
-# #          lty=c(1,1,1), col=c("red","blue","green"), cex=0.5)
-#   
-#   title(paste("m = ", m, sep=""))
-# }
-# 
-# plot(1, type = "n", axes=FALSE, xlab="", ylab="")
-# legend(x = "top",inset = 0,
-#        legend = c("Abar", "Phat_SVD", "Phat_Eig"), 
-#        col=colorVec, lwd=lineWidth, cex=1.5, horiz = TRUE)
-
-
-
-
-
 
 library(ggplot2)
 library(grid)
@@ -149,13 +83,36 @@ for (iM in 1:length(mVec)) {
 # layout <- matrix(1:4, nrow = 2, byrow = TRUE)
 # multiplot(plotlist = pp, layout = layout)
 
-grid.newpage()
-pushViewport(viewport(layout = grid.layout(3, 2, heights = unit(c(1, 8, 8), "null"))))
-grid.text(paste(dataName, ", n=", n, ", M=", M, sep=""),
-          vp = viewport(layout.pos.row = 1, layout.pos.col = 1:2),
-          gp=gpar(fontsize=30))
-for (iRow in 2:3) {
-  for (iCol in 1:2) {
-    print(pp[[(iRow-2)*2+iCol]],vp = viewport(layout.pos.row = iRow, layout.pos.col = iCol))
-  }
+# grid.newpage()
+# pushViewport(viewport(layout = grid.layout(3, 2, heights = unit(c(1, 8, 8), "null"))))
+# grid.text(paste(dataName, ", n=", n, ", M=", M, sep=""),
+#           vp = viewport(layout.pos.row = 1, layout.pos.col = 1:2),
+#           gp=gpar(fontsize=30))
+# for (iRow in 2:3) {
+#   for (iCol in 1:2) {
+#     print(pp[[(iRow-2)*2+iCol]], vp = viewport(layout.pos.row = iRow, layout.pos.col = iCol))
+#   }
+# }
+
+
+library(gridExtra)
+
+grid_arrange_shared_legend <- function(t, ...) {
+  plots <- list(...)
+  g <- ggplotGrob(plots[[1]] + theme(legend.position="bottom"))$grobs
+  legend <- g[[which(sapply(g, function(x) x$name) == "guide-box")]]
+  lheight <- sum(legend$height)
+  theight <- lheight*0.5
+  grid.arrange(
+    main=textGrob(t, gp=gpar(cex=1.5)),
+    do.call(arrangeGrob, lapply(plots, function(x)
+      x + theme(legend.position="none"))),
+    legend,
+    ncol = 1,
+    heights = unit.c(theight, unit(1, "npc") - lheight - theight, lheight))
 }
+
+grid_arrange_shared_legend(paste(dataName, ", n=", n, ", M=", M, sep=""), 
+                           pp[[1]], pp[[2]], pp[[3]], pp[[4]])
+
+
