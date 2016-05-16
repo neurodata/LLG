1b9eba708cb43b79da43744d81fbb50f02d8e65e
Runze Tang <tangrunze@gmail.com>
2016-05-16 14:58:59 -0400

    Figure for SBM example updated.

--- Code/R/sbm_example.R
+++ Code/R/sbm_example.R
@@ -13,22 +13,20 @@ B = array(c(0.9,0.27,0.05,0.1,0.3,
             0.1,0.26,0.25,0.7,0.18,
             0.3,0.14,0.33,0.18,0.58), dim=c(5,5))
 
-# tmp = runif(5)
-# tmp/sum(tmp)
 rho = c(0.22,0.39,0.05,0.16,0.18)
-n = 1000
+n = 200
 
 tau = rep(1:5,round(n*rho))
 P = B[tau,tau]
 diag(P) = 0
 
-image(Matrix(P),main=list(label="P",cex=2),sub="",xlab=list(cex=0),ylab=list(cex=0),
-      scales=list(x=list(draw=FALSE),y=list(draw=FALSE)))
+image(Matrix(P),main=list(label="Probability Matrix",cex=2),sub="",
+      xlab=list(cex=0),ylab=list(cex=0),scales=list(x=list(draw=FALSE),y=list(draw=FALSE)),
+      lwd=0)
 
 g = sample_sbm(n, B, round(n*rho), directed=F, loops=F)
 A = as_adj(g, type="both", sparse=FALSE)
 
-# image(Matrix(A),main=list(label="A",cex=2),sub="",xlab=list(cex=0),ylab=list(cex=0),
-#       scales=list(x=list(cex=2),y=list(cex=2)))
-image(Matrix(A),main=list(label="A",cex=2),sub="",xlab=list(cex=0),ylab=list(cex=0),
-      scales=list(x=list(draw=FALSE),y=list(draw=FALSE)))
+image(Matrix(A),main=list(label="Adjacency Matrix",cex=2),sub="",
+      xlab=list(cex=0),ylab=list(cex=0),scales=list(x=list(draw=FALSE),y=list(draw=FALSE)),
+      lwd=0)
