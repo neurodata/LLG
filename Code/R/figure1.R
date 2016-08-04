145ed506f2b110feb326733fa03d952ed700ca64
dpmcsuss@gmail.com <dsussman@wireless1x-168-122-15-187.bu.edu>
2016-08-04 09:51:52 -0400

    Added in Abar and Phat to first figure

--- Code/R/figure1.R
+++ Code/R/figure1.R
@@ -3,11 +3,13 @@ rm(list = ls())
 # setwd("/Users/Runze/Documents/GitHub/LLG/Code/R")
 # setwd("E:/GitHub/LLG/Code/R")
 # setwd("/cis/home/rtang/LLG/Code/R")
-
+library(extrafont)
 set.seed(12345)
 
 require(Matrix)
 require(igraph)
+
+setEPS()
 
 B = array(c(0.9,0.27,0.05,0.1,0.3,
             0.27,0.67,0.02,0.26,0.14,
@@ -16,6 +18,7 @@ B = array(c(0.9,0.27,0.05,0.1,0.3,
             0.3,0.14,0.33,0.18,0.58), dim=c(5,5))
 
 rho = c(0.22,0.39,0.05,0.16,0.18)
+K <- length(rho)
 n = 200
 
 tau = rep(1:5,round(n*rho))
@@ -28,6 +31,7 @@ image(Matrix(P),main=list(label="Probability Matrix"),sub="",
       lwd=0)
 dev.off()
 
+
 g = sample_sbm(n, B, round(n*rho), directed=F, loops=F)
 A = as_adj(g, type="both", sparse=FALSE)
 
@@ -36,3 +40,28 @@ image(Matrix(A),main=list(label="Adjacency Matrix"),sub="",
       xlab=list(cex=0),ylab=list(cex=0),scales=list(x=list(draw=FALSE),y=list(draw=FALSE)),
       lwd=0)
 dev.off()
+
+A2 = sample_sbm(n, B, round(n*rho), directed=F, loops=F)[]
+A3 = sample_sbm(n, B, round(n*rho), directed=F, loops=F)[]
+
+Abar <- (A+A2+A3)/3
+Phat <- as.matrix(with(eigen(Abar), vectors[,1:K]%*%diag(values[1:K])%*%t(vectors[,1:K])))
+Phat[Phat<0]<- 0
+Phat[Phat>1]<- 1
+
+
+pdf("../../Draft/SBM_Abar.pdf", family="CM Roman", width=4, height=4.4)
+image(Matrix(Abar),main=list(label="element-wise mean (M=3)"),sub="",
+      xlab=list(cex=0),ylab=list(cex=0),scales=list(x=list(draw=FALSE),y=list(draw=FALSE)),
+      lwd=0)
+dev.off()
+
+
+pdf("../../Draft/SBM_Phat.pdf", family="CM Roman", width=4, height=4.4)
+image(Matrix(Phat),main=list(label="rank-5 approximation (M=3)"),sub="",
+      xlab=list(cex=0),ylab=list(cex=0),scales=list(x=list(draw=FALSE),y=list(draw=FALSE)),
+      lwd=0)
+dev.off()
+
+
+
