d47ba90db513a82197a50e795f6bd6820eb27013
Runze Tang <tangrunze@gmail.com>
2017-01-16 11:38:16 -0500

    Revision updated.

--- Code/MATLAB/BrainVis/iossurfbrain_eigenvector.m
+++ Code/MATLAB/BrainVis/iossurfbrain_eigenvector.m
@@ -41,7 +41,7 @@ end
 
 figure(1);
 
-p1 = subplot(1,2,1);
+p1 = subplot(1,3,1);
 
 FV = {};
 FV{1} = isosurface(double(maskCopy~=0), .5);
@@ -70,21 +70,58 @@ for i = 1:70
     lighting gouraud
     material dull
 end
-colorbar
+% colorbar
 
 hold off
 
 
 
 
-p2 = subplot(1,2,2);
+
+p2 = subplot(1,3,2);
 
 FV = {};
 FV{1} = isosurface(double(maskCopy~=0), .5);
-colors = distinguishable_colors(21,'w');
+% colors = distinguishable_colors(71,'w');
 p = patch(FV{1});
 isonormals(double(maskCopy~=0),p)
-p.FaceColor = colors(1,:);
+p.FaceColor = [0, 0, 0];
+p.EdgeColor = 'none';
+p.FaceAlpha = .15;
+daspect([1,1,1])
+view([0,1,0]); axis tight
+lighting gouraud
+material dull
+light('Position', [100 100 100], 'Style', 'local')
+set(gca,'Visible','off')
+hold on
+
+for i = 1:70
+    V = double(maskCopy == i);
+    FV{i+1} = isosurface(V, .5);
+    p = patch(FV{i+1});
+    isonormals(double(maskCopy~=0),p)
+    p.FaceColor = [1, 1, (xHat(i)+1)/2];
+    p.EdgeColor = 'none';
+    p.FaceAlpha = 0.2;
+    lighting gouraud
+    material dull
+end
+% colorbar
+
+hold off
+
+
+
+
+p3 = subplot(1,3,3);
+
+FV = {};
+FV{1} = isosurface(double(maskCopy~=0), .5);
+% colors = distinguishable_colors(71,'w');
+p = patch(FV{1});
+isonormals(double(maskCopy~=0),p)
+p.FaceColor = [0, 0, 0];
 p.EdgeColor = 'none';
 p.FaceAlpha = .15;
 daspect([1,1,1])
@@ -95,51 +132,25 @@ light('Position', [100 100 100], 'Style', 'local')
 set(gca,'Visible','off')
 hold on
 
-for i = 1:length(vertexVec)
-    V = double(mask.img == vertexVec(i));
+for i = 1:70
+    V = double(maskCopy == i);
     FV{i+1} = isosurface(V, .5);
     p = patch(FV{i+1});
     isonormals(double(maskCopy~=0),p)
-    p.FaceColor = colors(i+1,:);
+    p.FaceColor = [1, 1, (xHat(i)+1)/2];
     p.EdgeColor = 'none';
-    p.FaceAlpha = 1;
+    p.FaceAlpha = 0.2;
     lighting gouraud
     material dull
 end
+% colorbar
 
-hold on;
-
-for i = 1:size(edgeVec,1)
-    cen = [centroids((edgeVec(i,1)),:);
-        centroids((edgeVec(i,2)),:)];
-    if (edgeMagnitude(i) > 0)
-        pp3 = plot3(cen(:,2), cen(:,1), cen(:,3), '-r', ...
-            'LineWidth', edgeMagnitude(i));
-    else
-        pp4 = plot3(cen(:,2), cen(:,1), cen(:,3), '-b', ...
-            'LineWidth', -edgeMagnitude(i));
-    end
-end
 hold off
 
-hL = legend([pp1,pp2], {'Overestimate','Underestimate'}, ...
-    'Location','South');
-set(hL,'FontSize',20)
 
-p1Pos = get(p1,'position');
-p2Pos = get(p2,'position');
-p2Pos(2:4) = p1Pos(2:4);
-set(p2, 'position', p2Pos);
 
-% if (model == 1)
-%     figureName = '../../../Result/Diff_Between_desikan.png';
-% elseif (model == 2)
-%     figureName = '../../../Result/Diff_Abar_desikan.png';
-% else
-%     figureName = '../../../Result/Diff_Phat_desikan.png';
-% end
-% 
-% r = 150; % pixels per inch
-% set(gcf, 'PaperUnits', 'inches', 'PaperPosition', [0 0 1080 480]/r);
-% print(gcf,'-dpng',sprintf('-r%d',r), figureName);
 
+% p1Pos = get(p1,'position');
+% p2Pos = get(p2,'position');
+% p2Pos(2:4) = p1Pos(2:4);
+% set(p2, 'position', p2Pos);
